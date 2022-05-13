59005c14162a45770f78672ce2ead7813b6ea75e
Luis Gustavo F Freire <99625415+luisgustavo2022@users.noreply.github.com>
2022-05-13 20:29:12 -0300

    jogo completo
    coloquei pontuação e dificuldade

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
+import random
+
 print('bem vindo o o jogo de advinhação')
 
-n_s = 43
-chances = 3
+n_s = round(random.randrange(1, 100))
+print('qula o nivem de dificuldade?')
+print('(1)fácil  (2)médio  (3)difícil')
+nivel = int(input('defina o nível:'))
 
-while chances > 0:
-    chutestr = input('adivinhe o número: ')
+facil = nivel == 1
+medio = nivel == 2
+dificil = nivel == 3
+chance = 1000
+score = 0
+if facil:
+    chance = 20
+elif medio:
+    chance = 10
+else:
+    chance = 5
+
+while chance > 0:
+    chutestr = input('Adivinhe o número entre 1 e 100: ')
     chute = int(chutestr)
     acertou = chute == n_s
     maior = chute > n_s
     menor = chute < n_s
-    if acertou:
+    if chute < 1 or chute > 100:
+        print("coloque um número entre 1 e 100")
+        continue
+    elif acertou:
         print("você acertou")
-        chances = 0
+        chance = 0
+        print("você fez {} de pontuação.".format(score))
     else:
         if maior:
             print("você errou!, o número é menor.")
-            chances = chances - 1
+            chance = chance - 1
+            print("você tem mais {} chances".format(chance))
         elif menor:
             print("você errou!, o número é maior.")
-            chances = chances - 1
+            print("você tem mais {} chances".format(chance))
+    chance = chance - 1
+    perdidos = abs(chute - n_s)
+    score = score - perdidos
 
-
-
-
-    print("fim do jogo")
+print("o número era ", n_s)
+print("fim do jogo")
