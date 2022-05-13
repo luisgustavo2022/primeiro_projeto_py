22a02fe389fda1eb6d00204982bd10d09d9e9f59
Luis Gustavo F Freire <99625415+luisgustavo2022@users.noreply.github.com>
2022-05-13 00:36:01 -0300

    more details
    coloquei mais detalhes sobre o chute se foi maior ou menor

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ print('bem vindo o o jogo de advinhação')
 n_s = 43
 chutestr = input('adivinhe o número: ')
 chute = int(chutestr)
-if n_s == chute:
+acertou = chute == n_s
+maior = chute > n_s
+menor = chute < n_s
+if acertou:
     print("você acertou")
-else :
-    print("você errou!")
+else:
+    if maior :
+        print("você errou!, o número é menor.")
+    elif menor :
+        print("você errou!, o número é maior.")
 
 print("fim do jogo")
