760d15273d60cba9d92b88552c176519bc5cbb15
Luis Gustavo F Freire <99625415+luisgustavo2022@users.noreply.github.com>
2022-05-13 02:04:17 -0300

    more chances
     tentar mais de uma vez adivinhar

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 print('bem vindo o o jogo de advinhação')
 
 n_s = 43
-chutestr = input('adivinhe o número: ')
-chute = int(chutestr)
-acertou = chute == n_s
-maior = chute > n_s
-menor = chute < n_s
-if acertou:
-    print("você acertou")
-else:
-    if maior :
-        print("você errou!, o número é menor.")
-    elif menor :
-        print("você errou!, o número é maior.")
+chances = 3
 
-print("fim do jogo")
+while chances > 0:
+    chutestr = input('adivinhe o número: ')
+    chute = int(chutestr)
+    acertou = chute == n_s
+    maior = chute > n_s
+    menor = chute < n_s
+    if acertou:
+        print("você acertou")
+        chances = 0
+    else:
+        if maior:
+            print("você errou!, o número é menor.")
+            chances = chances - 1
+        elif menor:
+            print("você errou!, o número é maior.")
+            chances = chances - 1
+
+
+
+
+    print("fim do jogo")
