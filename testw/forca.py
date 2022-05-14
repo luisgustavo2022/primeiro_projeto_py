ec48c5fa5ad49f8e3754243e0b21db266da86e1a
Luis Gustavo F Freire <99625415+luisgustavo2022@users.noreply.github.com>
2022-05-14 02:06:06 -0300

    pseudo forca

--- testw/forca.py
+++ testw/forca.py
@@ -1,16 +1,29 @@
 def jogar():
-    print('jogo da forca"')
+    palavra_secreta = ["m", "a", "c", "e", "i", "o"]
+    letras_descobertas = []
 
+    print("jogo da forca")
 
+    for i in range(0, len(palavra_secreta)):
+        letras_descobertas.append("-")
 
+    acertou = False
 
+    while acertou is False:
+        letra = str(input("digite a letra:"))
 
+        for i in range(0, len(palavra_secreta)):
+            if letra == palavra_secreta[i]:
+                letras_descobertas[i] = letra
 
+            print(letras_descobertas[i])
+            acertou = True
 
+        for x in range(0, len(letras_descobertas)):
+            if letras_descobertas[x] == "-":
+                acertou = False
 
-
-
-
+    print("você acertoou a palavra, parábens")
 
 if __name__ == "__main__":
     jogar()
